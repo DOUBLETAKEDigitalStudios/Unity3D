82c2cf4c4c8780a17927b248616f009b3817bd28
//DOUBLETAKE <contact@doubletakedigitalstudios.com>
2022-07-17 17:25:34 -0400

    Create PlayerController.cs

---
+++ DemoGame1/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField]
+    float _moveSpeed = 10f;
+    const string HORIZONTAL = "Horizontal";
+    const string VERTICAL = "Vertical";
+
+    #endregion Fields
+
+    #region Methods
+
+    // Callback: Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        //User input
+        float xValue = Input.GetAxis(HORIZONTAL) * Time.deltaTime * _moveSpeed;
+        float zValue = Input.GetAxis(VERTICAL) * Time.deltaTime * _moveSpeed;
+
+        transform.Translate(xValue, 0f, zValue);
+    }
+
+    #endregion Methods
+}
